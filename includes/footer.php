7567620e36b53cbd1f5a600b43e16325e7173e0f
Antonio de Jesús Gonzaga Cruz <gonzagaantonio012@gmail.com>
2025-12-27 14:52:08 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer class="site-footer bg-dark text-light py-4">
+  <div class="container text-center small">
+    <p class="mb-1">© <?php echo date('Y'); ?> Antonio de Jesús Gonzaga Cruz</p>
+
+    <p class="mb-0">
+<p class="mb-0">
+  <a href="https://www.linkedin.com/in/antonio-de-jesus-gonzaga-cruz-388b15398"
+     target="_blank"
+     class="text-light me-3">
+    <i class="bi bi-linkedin"></i> LinkedIn
+  </a>
+
+  <a href="https://github.com/TU_USUARIO"
+     target="_blank"
+     class="text-light">
+    <i class="bi bi-github"></i> GitHub
+  </a>
+    </p>
+  </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<nav id="mainNav" class="navbar navbar-expand-lg navbar-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand d-flex align-items-center">
+      <img src="assets/img/logo.png" alt="logo" style="height:28px;margin-right:10px;">
+      <span class="brand-text">TONY_DEV</span>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navMenu">
+      <ul class="navbar-nav ms-auto align-items-center">
+        <li class="nav-item"><a class="nav-link text-uppercase" href="#about">Acerca de</a></li>
+        <li class="nav-item"><a class="nav-link text-uppercase" href="#pasion">Pasión</a></li>
+        <li class="nav-item"><a class="nav-link text-uppercase" href="#experiencie">Experiencia</a></li>
+        <li class="nav-item"><a class="nav-link text-uppercase" href="#projects">Proyectos</a></li>
+        <li class="nav-item"><a class="nav-link text-uppercase" href="#contact">Contacto</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
